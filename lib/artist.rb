4f5c35d22359e8970a47b2a4bf4664a5c0f8ec8e
Jonathan Hong <jonhong777@gmail.com>
2018-07-12 16:40:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,17 +9,20 @@ class Artist
     @songs = []
   end
   
+  # take in a song, add it to the @songs array and relate it to the artist
   def add_song(song)
     @songs << song
     song.artist = self
   end
   
+  # create a new song instance, add it to the @songs array, and relate to artist
   def add_song_by_name(song_name)
     new_song = Song.new(song_name)
     new_song.artist = self
     @songs << new_song
   end
   
+  # count the total number of songs
   def self.song_count
     Song.all.count
   end
