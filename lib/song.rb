8ecbc42dbe04ce19069f53749ad859182c99661d
Jonathan Hong <jonhong777@gmail.com>
2018-07-12 16:27:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,13 @@
 class Song 
-  attr_accessor :name
+  attr_accessor :name, :artist
+  
   @@all = []
   def initialize(name)
     @name = name
     @@all << self
+  end
+  
+  def artist_name
+    self.artist
   end
 end
