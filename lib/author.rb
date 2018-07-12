49edcee1dae4cd12b9f462330acfc2d04aabd406
Jonathan Hong <jonhong777@gmail.com>
2018-07-12 16:51:09 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,6 +11,12 @@ class Author
     new_post.author = self
   end
   
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    new_post.author = self 
+    @posts << new_post
+  end
+  
   def self.post_count
     Post.all.count
   end
